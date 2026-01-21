b2af10fc467b4c09cb601ccf4b057c96efdb42d5
Ian Philip Eglin <philip@eglin.no>
2026-01-21 23:00:43 +0100

    feat: adding TCP dataset cloning and verification

--- crates/tcp_preprocess/src/lib.rs
+++ crates/tcp_preprocess/src/lib.rs
@@ -1,17 +1,77 @@
 use anyhow::Result;
 use config::TCPPreprocessConfig;
+use config::annex;
+use git2::Repository;
+use std::path::PathBuf;
+use thiserror::Error;
+use tracing::info;
 
-pub fn run(cfg: &TCPPreprocessConfig) -> Result<()> {
-    print_config(cfg);
-
-    Ok(())
+#[derive(Error, Debug)]
+pub enum TCPPreprocessError {
+    #[error("File already exists: {0}")]
+    AlreadyExists(String),
+    #[error("Required file missing: {0}")]
+    RequiredFileMissing(String),
 }
 
-fn print_config(cfg: &TCPPreprocessConfig) {
-    println!("TPC Preprocessing:");
-    println!("  fMRI Dir: {}", cfg.fmri_dir.display());
-    println!("  TCP Dir: {}", cfg.tcp_dir.display());
-    println!("  Output Dir: {}", cfg.output_dir.display());
-    println!("  Filters: {:?}", cfg.filters);
-    println!("  Dry run: {}", cfg.dry_run);
+pub fn run(cfg: &TCPPreprocessConfig) -> Result<()> {
+    info!("{:?}", cfg);
+
+    ////////////////////////
+    // Initialize dataset //
+    ////////////////////////
+
+    // Clone dataset
+    if !cfg.tcp_dir.is_dir() {
+        let dataset_url = "https://github.com/OpenNeuroDatasets/ds005237.git";
+        let local_path = &cfg.tcp_dir;
+
+        if !cfg.dry_run {
+            if let Some(parent) = local_path.parent() {
+                std::fs::create_dir_all(parent)?;
+            }
+            info!("Cloning {} into {}...", dataset_url, local_path.display());
+            match Repository::clone(dataset_url, local_path) {
+                Ok(_repo) => info!("Cloned successfully!"),
+                Err(e) => panic!("failed to clone: {}", e),
+            };
+        } else {
+            info!("Skipped cloning. Dry_run config detected")
+        }
+    }
+    info!("TCP Dataset available on: {}", cfg.tcp_dir.display());
+
+    // Validate and set annex remote
+    if annex::validate_remote(&cfg.tcp_dir, &cfg.tcp_annex_remote).is_err() {
+        annex::enable_remote(&cfg.tcp_dir, &cfg.tcp_annex_remote)?;
+    }
+    info!("Validated annex remote: {}", &cfg.tcp_annex_remote);
+
+    // Validate dataset
+    let phenotype_dir = &cfg.tcp_dir.join("phenotype");
+    let required_files: Vec<PathBuf> = vec![
+        phenotype_dir.join("demos.tsv"),
+        phenotype_dir.join("shaps01.tsv"),
+    ];
+    required_files.iter().try_for_each(|file_path| {
+        if !file_path.is_file() && !annex::is_broken_symlink(file_path) {
+            return Err(TCPPreprocessError::RequiredFileMissing(format!(
+                "{}",
+                file_path.display()
+            )));
+        }
+        // Continue to next iteration
+        Ok(())
+    })?;
+    info!("All required files located: {:?}", required_files);
+
+    ///////////////////
+    // Apply filters //
+    ///////////////////
+
+    //////////////////////////////
+    // Establish subject groups //
+    //////////////////////////////
+
+    Ok(())
 }
