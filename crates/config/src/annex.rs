e4e42d912e960a77cd3e799b642b4655f8920f80
Ian Philip Eglin <philip@eglin.no>
2026-01-21 22:59:27 +0100

    feat: adding git annex functions

--- crates/config/src/annex.rs
+++ crates/config/src/annex.rs
@@ -20,6 +20,10 @@ pub enum AnnexError {
     MissingSymlinkMetadata(String),
     #[error("Failed to get file from annex: {0}")]
     FetchError(String),
+    #[error("Remote not found: {0}")]
+    RemoteNotFound(String),
+    #[error("Failed to enable annex remote: {0}")]
+    RemoteEnablingFailed(String),
 }
 
 pub fn is_broken_symlink<P: AsRef<Path>>(path: P) -> bool {
@@ -44,7 +48,7 @@ pub fn get_file_from_annex<P: AsRef<Path>>(
     local_remote: P,
     symlink_path: P,
 ) -> Result<(), AnnexError> {
-    let remote = local_remote.as_ref();
+    let local = local_remote.as_ref();
     let symlink = symlink_path.as_ref();
 
     if symlink.exists() {
@@ -59,7 +63,7 @@ pub fn get_file_from_annex<P: AsRef<Path>>(
 
     let status = Command::new("git")
         .arg("-C")
-        .arg(remote)
+        .arg(local)
         .arg("annex")
         .arg("get")
         .arg(symlink)
@@ -74,7 +78,7 @@ pub fn get_file_from_annex<P: AsRef<Path>>(
 }
 
 pub fn drop_file<P: AsRef<Path>>(local_remote: P, symlink_path: P) -> Result<(), AnnexError> {
-    let remote = local_remote.as_ref();
+    let local = local_remote.as_ref();
     let symlink = symlink_path.as_ref();
 
     if !symlink.exists() {
@@ -86,7 +90,7 @@ pub fn drop_file<P: AsRef<Path>>(local_remote: P, symlink_path: P) -> Result<(),
 
     let status = Command::new("git")
         .arg("-C")
-        .arg(remote)
+        .arg(local)
         .arg("annex")
         .arg("drop")
         .arg(symlink)
@@ -95,6 +99,66 @@ pub fn drop_file<P: AsRef<Path>>(local_remote: P, symlink_path: P) -> Result<(),
 
     if !status.success() {
         return Err(AnnexError::FetchError(format!("{}", symlink.display())));
+    }
+
+    Ok(())
+}
+
+pub fn enable_remote<P: AsRef<Path>>(
+    local_remote: P,
+    annex_remote_name: &String,
+) -> Result<(), AnnexError> {
+    let local_remote = local_remote.as_ref();
+
+    let output = Command::new("git")
+        .arg("-C")
+        .arg(local_remote)
+        .arg("annex")
+        .arg("enableremote")
+        .arg(annex_remote_name)
+        .output()
+        .expect("failed to execute git-annex");
+    let stdout_str = String::from_utf8_lossy(&output.stdout);
+    let expected_line = String::from("available: true");
+
+    if !stdout_str.contains(&expected_line) {
+        return Err(AnnexError::RemoteNotFound(annex_remote_name.to_string()));
+    }
+
+    if !output.status.success() {
+        return Err(AnnexError::RemoteEnablingFailed(
+            annex_remote_name.to_string(),
+        ));
+    }
+
+    Ok(())
+}
+
+pub fn validate_remote<P: AsRef<Path>>(
+    local_remote: P,
+    annex_remote_name: &String,
+) -> Result<(), AnnexError> {
+    let local_remote = local_remote.as_ref();
+
+    let output = Command::new("git")
+        .arg("-C")
+        .arg(local_remote)
+        .arg("annex")
+        .arg("info")
+        .arg(annex_remote_name)
+        .output()
+        .expect("failed to validate git-annex remote");
+    let stdout_str = String::from_utf8_lossy(&output.stdout);
+    let expected_line = String::from("available: true");
+
+    if !stdout_str.contains(&expected_line) {
+        return Err(AnnexError::RemoteNotFound(annex_remote_name.to_string()));
+    }
+
+    if !output.status.success() {
+        return Err(AnnexError::RemoteEnablingFailed(
+            annex_remote_name.to_string(),
+        ));
     }
 
     Ok(())
