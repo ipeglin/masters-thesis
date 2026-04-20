2b3d329cc08d5ae4b9a2bda78238101c5fbc5d9e
ipeglin <philip@eglin.no>
2026-04-20 17:22:19 +0200

    refactor: removing tcp directory from pipeline storage

--- scripts/sys-idun_config.sh
+++ scripts/sys-idun_config.sh
@@ -16,12 +16,12 @@ log_info "Configuring IDUN specific paths in config.toml"
 sed -i.bak \
     -e "s|^tcp_repo_dir = .*|tcp_repo_dir = \"/cluster/work/$USER/ds005237\"|" \
     -e "s|^fmriprep_output_dir = .*|fmriprep_output_dir = \"/cluster/projects/itea_lille-ie/Transdiagnostic/output/fmriprep-25.1.4\"|" \
-    -e "s|^parcellated_ts_dir = .*|parcellated_ts_dir = \"/cluster/work/$USER/preprocessing/tcp/parcellations\"|" \
-    -e "s|^subject_filter_dir = .*|subject_filter_dir = \"/cluster/work/$USER/preprocessing/tcp/subject_filters\"|" \
-    -e "s|^task_regressors_output_dir = .*|task_regressors_output_dir = \"/cluster/work/$USER/preprocessing/tcp/task_regressors\"|" \
-    -e "s|^training_subjects_path = .*|training_subjects_path = \"/cluster/work/$USER/processing/tcp/classifier_models/subjects_train.csv\"|" \
-    -e "s|^test_subjects_path = .*|test_subjects_path = \"/cluster/work/$USER/processing/tcp/classifier_models/subjects_test.csv\"|" \
-    -e "s|^validation_subjects_path = .*|validation_subjects_path = \"/cluster/work/$USER/processing/tcp/classifier_models/subjects_validation.csv\"|" \
+    -e "s|^parcellated_ts_dir = .*|parcellated_ts_dir = \"/cluster/work/$USER/preprocessing/parcellations\"|" \
+    -e "s|^subject_filter_dir = .*|subject_filter_dir = \"/cluster/work/$USER/preprocessing/subject_filters\"|" \
+    -e "s|^task_regressors_output_dir = .*|task_regressors_output_dir = \"/cluster/work/$USER/preprocessing/task_regressors\"|" \
+    -e "s|^training_subjects_path = .*|training_subjects_path = \"/cluster/work/$USER/processing/classifier_models/subjects_train.csv\"|" \
+    -e "s|^test_subjects_path = .*|test_subjects_path = \"/cluster/work/$USER/processing/classifier_models/subjects_test.csv\"|" \
+    -e "s|^validation_subjects_path = .*|validation_subjects_path = \"/cluster/work/$USER/processing/classifier_models/subjects_validation.csv\"|" \
     "$CONFIG_FILE"
 
 rm -f "${CONFIG_FILE}.bak"
